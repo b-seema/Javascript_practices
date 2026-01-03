597af8da52af2f749a38becd0a70167cf4db6654
unknown <seemabhardwaj916@gmail.com>
2026-01-03 13:27:07 +0530

    Initial commit

---
+++ basics.js
@@ -0,0 +1,13 @@
+console.log("Hello world");
+let a=1
+console.log(a)
+console.log(typeof(a))
+
+let b = 2.3 
+console.log(typeof(b))
+
+let g= "seema "
+console.log(typeof(g))
+
+let value = false 
+console.log(typeof(value))
